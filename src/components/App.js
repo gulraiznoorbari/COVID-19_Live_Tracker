e8f841d10b99e1d8d52ca7dd7985a44efe2846c4
Gulraiz Noor Bari ⚡ <78252747+gulraiznoorbari@users.noreply.github.com>
2021-12-26 22:46:26 +0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <h1> React App </h1>
+            <h4> Using Webpack and Babel with Hot Reload</h4>
+        </>
+    );
+}
+
+export default App;
